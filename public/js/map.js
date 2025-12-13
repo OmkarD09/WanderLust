58533374df82c5d77b71b89202fa9a08a37d22e0
OmkarD09 <omkardhakane09@gmail.com>
2025-12-13 16:40:09 +0530

    Maps

--- public/js/map.js
+++ public/js/map.js
@@ -2,11 +2,11 @@
 
         const map = new mapboxgl.Map({
             container: 'map', // container ID
-            center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+            center: JSON.parse(coordinates), // starting position [lng, lat]. Note that lat must be set between -90 and 90
             zoom: 12 // starting zoom
             });
             
 
             const marker = new mapboxgl.Marker( {color: 'red'})
-            .setLngLat(coordinates)
+            .setLngLat(JSON.parse(coordinates))
             .addTo(map);
